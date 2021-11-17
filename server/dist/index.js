e727e1dadf6560665f148636743330fa17d20976
Adrian Zinko <adrian.zinko@billennium.com>
2021-11-17 23:56:39 +0100

    setup connection with db

--- server/dist/index.js
+++ server/dist/index.js
@@ -1,2 +1,19 @@
-console.log("hi");
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const mongoose_1 = __importDefault(require("mongoose"));
+const cors_1 = __importDefault(require("cors"));
+const dotenv_1 = __importDefault(require("dotenv"));
+const app = (0, express_1.default)();
+dotenv_1.default.config();
+app.use(express_1.default.json());
+app.use((0, cors_1.default)({ origin: "localhost:3000", credentials: true }));
+const PORT = 5000;
+mongoose_1.default
+    .connect(process.env.MONGO_URL)
+    .then(() => app.listen(PORT, () => console.log(`Server is listening at http:localhost:${PORT}`)))
+    .catch((error) => console.error(error.message));
 //# sourceMappingURL=index.js.map
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,5 +1,21 @@
-// import express from "express";
-// import mongoose from "mongoose";
-// import cors from "cors";
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
 
-console.log("hi");
+const app = express();
+
+dotenv.config();
+
+app.use(express.json());
+app.use(cors({ origin: "localhost:3000", credentials: true }));
+
+const PORT = 5000;
+mongoose
+  .connect(process.env.MONGO_URL as string)
+  .then(() =>
+    app.listen(PORT, () =>
+      console.log(`Server is listening at http://localhost:${PORT}`)
+    )
+  )
+  .catch((error) => console.error(error.message));
