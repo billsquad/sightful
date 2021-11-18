dfb6dc87ad5a3fe2ec366c1e42d0fcbf8cb62a30
Adrian Zinko <adrian.zinko@billennium.com>
2021-11-18 23:56:49 +0100

    deploy on heroku

--- server/dist/index.js
+++ server/dist/index.js
@@ -7,13 +7,18 @@ const express_1 = __importDefault(require("express"));
 const mongoose_1 = __importDefault(require("mongoose"));
 const cors_1 = __importDefault(require("cors"));
 const dotenv_1 = __importDefault(require("dotenv"));
+const articles_1 = __importDefault(require("./routes/articles"));
 const app = (0, express_1.default)();
 dotenv_1.default.config();
 app.use(express_1.default.json());
-app.use((0, cors_1.default)({ origin: "localhost:3000", credentials: true }));
-const PORT = 5000;
+app.use((0, cors_1.default)({ origin: "http://localhost:3000", credentials: true }));
+app.use("/articles", articles_1.default);
+app.get("/", (_, res) => {
+    res.send("Sightful API");
+});
+const PORT = process.env.PORT || 5000;
 mongoose_1.default
     .connect(process.env.MONGO_URL)
-    .then(() => app.listen(PORT, () => console.log(`Server is listening at http:localhost:${PORT}`)))
+    .then(() => app.listen(PORT, () => console.log(`Server is listening at http://localhost:${PORT}`)))
     .catch((error) => console.error(error.message));
 //# sourceMappingURL=index.js.map
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,20 +1,25 @@
-import express from "express";
+import express, { Response } from "express";
 import mongoose from "mongoose";
 import cors from "cors";
 import dotenv from "dotenv";
+
 import articleRoutes from "./routes/articles";
 
 const app = express();
 
-dotenv.config();
+dotenv.config({ path: __dirname + "/.env" });
 
 app.use(express.json());
 app.use(cors({ origin: "http://localhost:3000", credentials: true }));
 app.use("/articles", articleRoutes);
 
-const PORT = 5000;
+app.get("/", (_, res: Response) => {
+  res.send("Sightful API");
+});
+
+const PORT = process.env.PORT || 5000;
 mongoose
-  .connect(process.env.MONGO_URL as string)
+  .connect(process.env.MONGO_URI as string)
   .then(() =>
     app.listen(PORT, () =>
       console.log(`Server is listening at http://localhost:${PORT}`)
