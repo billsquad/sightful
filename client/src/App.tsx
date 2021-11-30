947a2980f82ec01a0e49fb3217cdeb163716cd88
Adrian Zinko <adrian.zinko@billennium.com>
2021-11-30 15:30:05 +0100

    prevent logged user to access auth route

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,21 +1,30 @@
-import { Route } from "wouter";
+import { Redirect, Route } from "wouter";
 
 import { Navbar } from "./components/Navbar/Navbar";
 import { Home } from "./components/Home/Home";
 import { Auth } from "./components/Auth/Auth";
+import { ArticlePage } from "./components/ArticlePage/ArticlePage";
 
 // TODO: change font style
 // TODO: Refactor to tailwindcss
 // TODO: Add form available from modal window
 
-function App() {
+const App = () => {
+  const user = JSON.parse(localStorage.getItem("sessionId") as string);
+
   return (
     <>
       <Navbar />
-      <Route path="/" component={Home} />
-      <Route path="/auth" component={Auth} />
+      <Route path="/" component={() => <Redirect to="/articles" />} />
+      <Route path="/articles" component={Home} />
+      <Route path="/articles/search" component={Home} />
+      <Route path="/articles/:id" component={ArticlePage} />
+      <Route
+        path="/auth"
+        component={() => (!user ? <Auth /> : <Redirect to="/articles" />)}
+      />
     </>
   );
-}
+};
 
 export default App;
---
+++ client/src/components/ArticlePage/ArticlePage.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+interface ArticlePageProps {}
+
+export const ArticlePage: React.FC<ArticlePageProps> = ({}) => {
+  console.log("article page");
+
+  return (
+    <>
+      <div>article page</div>
+    </>
+  );
+};
