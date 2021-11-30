c7240f06132a830aae62580bbe297a60ba1bcc9f
Adrian Zinko <adrian.zinko@billennium.com>
2021-11-30 16:29:40 +0100

    adjust articles

--- client/src/components/Articles/Articles.tsx
+++ client/src/components/Articles/Articles.tsx
@@ -5,7 +5,7 @@ import useStyles from "./styles";
 import { Dispatch, SetStateAction } from "react";
 import { ArticleProps } from "./Article/interface";
 import { RootState } from "../../store";
-import { useAppSelector } from "../../hooks";
+import { useAppSelector } from "../../custom.hooks";
 
 export const Articles: React.FC<{
   setCurrentId: Dispatch<SetStateAction<null>>;
@@ -25,7 +25,7 @@ export const Articles: React.FC<{
       spacing={3}
     >
       {articles.map((article: ArticleProps) => (
-        <Grid key={article._id} item xs={12}>
+        <Grid key={article._id} item xs={12} sm={12} md={6} lg={6}>
           <Article article={article} setCurrentId={setCurrentId} />
         </Grid>
       ))}
